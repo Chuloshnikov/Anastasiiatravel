6c33a08338f5f16367195d9492f0c436c6494873
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-08-27 16:42:29 +0300

    started

---
+++ components/SpecialAbout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/cinzel.module.css';
+
+const SpecialAbout = () => {
+  return (
+    <div
+    className='text-center bg-[#749CBA]  pt-24 pb-12'
+    >
+      <h2
+      className={`${styles.cinzelFont} font-base tracking-widest text-[62px] text-yellow-500 mb-24`}
+      >
+        ЧОМУ САМЕ МИ?
+      </h2>
+
+    </div>
+  )
+}
+
+export default SpecialAbout;
