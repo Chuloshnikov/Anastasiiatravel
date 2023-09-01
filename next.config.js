f933871b85fba7ba39b562f4ea3678abbba928d4
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-01 18:53:50 +0300

    done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co", "images.pexels.com", "lh3.googleusercontent.com", "res.cloudinary.com", "craigourparkprimary.files.wordpress.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
 module.exports = nextConfig
