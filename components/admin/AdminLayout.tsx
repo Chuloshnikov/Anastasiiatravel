be36e28ec69397ad36b444eeb01369a7813aec61
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-03 21:08:02 +0300

    done

--- components/admin/AdminLayout.tsx
+++ components/admin/AdminLayout.tsx
@@ -6,12 +6,12 @@ import {  FaSignOutAlt } from "react-icons/fa";
 const AdminLayout = ({ children } :any ) => {
   return (
     <div className='mx-auto max-h-screen w-full flex flex-col bg-[#749CBA]'>
-        <div className='flex flex-col mt-[10%] max-w-[300px] mx-auto'>
+        <div className='flex flex-col xs:mt-[30%] mdl:mt-[15%] max-w-[300px] mx-auto'>
             <nav
             
             >
                 <ul 
-                className='flex items-center justify-center gap-2 bg-[#408692] text-yellow-950 mt-[5%] py-5 px-10'>
+                className='flex items-center justify-center gap-5 bg-[#408692] text-yellow-950 mt-[5%] py-5 px-10'>
                     <li>
                         <Link 
                         className='text-white font-semibold underline'
