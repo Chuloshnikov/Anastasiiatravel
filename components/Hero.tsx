655017599444d421279edceb97d2cfa8aadc7fbb
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-09-10 21:27:16 +0300

    done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,12 +15,12 @@ const Hero = () => {
         className='flex text-center justify-center'
         >
           <h1
-          className={`${styles.cinzelFont} tracking-widest absolute z-10 font-base xs:text-[12px] md:text-[44px] xl:text-[68px] xs:top-[11%] md:top-[22%] lg:top-[38%] text-yellow-500`}
+          className={`${styles.cinzelFont} tracking-widest absolute z-10 font-base xs:text-[12px] md:text-[44px] xl:text-[68px] xs:top-[16%] md:top-[22%] lg:top-[38%] text-yellow-500`}
           >
               ПОДОРОЖУЙ РАЗОМ З НАМИ
           </h1>
             <Link 
-              className='absolute z-10 xs:top-[15%] md:top-[38%] mdl:top-[35%] lg:top-[52%] xs:text-lg mdl:text-4xl tracking-tight font-normal 
+              className='absolute z-10 xs:top-[20%] md:top-[38%] mdl:top-[35%] lg:top-[52%] xs:text-lg mdl:text-4xl tracking-tight font-normal 
               xs:px-4 mdl:px-8 xs:py-2 mdl:py-4 border-2 border-yellow-500 hover:border-white hover:text-yellow-500 duration-300 rounded-2xl'
               href="/travels"
             >
